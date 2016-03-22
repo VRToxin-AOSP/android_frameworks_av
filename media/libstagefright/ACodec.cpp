825abd6341584514301b04bec47463580cc8e586
Yogesh Agrawal <yagrawal@gpartner-nvidia.com>
2016-03-22 12:37:28 +0530

    Nuplayer: Set PCM audio output format.
    By default always the audio output format is set to PCM 16 bit.
    Setting the appropriate PCM audio output format based on
    bits per sample.
    
    Change-Id: Id6235fe178f63f7dbdc718df7e7d4d078b54c0c4

--- media/libstagefright/ACodec.cpp
+++ media/libstagefright/ACodec.cpp
@@ -2149,6 +2149,12 @@ status_t ACodec::configureCodec(
         mChannelMaskPresent = false;
     }
 
+    if (msg->findInt32("bits-persample", &mBitsPerSample)) {
+        mBitsPerSamplePresent = true;
+    } else {
+        mBitsPerSamplePresent = false;
+    }
+
     int32_t maxInputSize;
     if (msg->findInt32("max-input-size", &maxInputSize)) {
         err = setMinBufferSize(kPortIndexInput, (size_t)maxInputSize);
@@ -4203,6 +4209,9 @@ status_t ACodec::getPortFormat(OMX_U32 portIndex, sp<AMessage> &notify) {
 
                     if (mChannelMaskPresent) {
                         notify->setInt32("channel-mask", mChannelMask);
+                    }
+                    if (mBitsPerSamplePresent) {
+                        notify->setInt32("bits-persample", mBitsPerSample);
                     }
                     break;
                 }
